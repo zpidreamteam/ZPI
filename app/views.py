f3455fba467ae615ccc2b9f6eeafd8417d386541
speedcom <mateusz.maciaszekhpc@gmail.com>
2014-10-08 18:56:56 +0200

    [#4] Working base of login form

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -7,5 +8,17 @@ def index():
     user = {'nickname': 'MC'}
 
     return render_template('index.html',
-                           title='Home',
+                           title='Strona glowna',
                            user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Mail="%s", remember_me=%s' %
+              (form.mail.data, str(form.remember_me.data)))
+        return redirect('/index')
+
+    return render_template('login.html',
+                           title='Logowanie',
+                           form=form)
