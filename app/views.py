38354dc6bd706697e7c71f1a2e937b1f7c12d890
speedcom <mateusz.maciaszekhpc@gmail.com>
2014-10-08 18:18:31 +0200

    [#2] Base sceleton page with one simple mock data to test a particular view

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
+from flask import render_template, flash, redirect
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'MC'}
+
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
