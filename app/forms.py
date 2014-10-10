9fb202e5dde241cfdd1a2a947eea078777f8ae70
speedcom <mateusz.maciaszekhpc@gmail.com>
2014-10-10 19:31:57 +0200

    [#4] Working base registration with login/logout

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,11 @@ from wtforms import StringField, BooleanField, PasswordField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
-    mail = StringField('mail', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
+
+class RegisterForm(Form):
+    email = StringField('email', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    password_repeat = PasswordField('password_repeat', validators=[DataRequired()])
--- app/models.py
+++ app/models.py
@@ -1,8 +1,32 @@
 from app import db
+from passlib.apps import custom_app_context as pwd_context
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2
+        except NameError:
+            return str(self.id)  # python 3
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
