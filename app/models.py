5354893f9b8164b14b832f00d29015b3cfd77396
speedcom <mateusz.maciaszekhpc@gmail.com>
2014-10-15 18:58:27 +0200

    [#11] init category model

--- app/models.py
+++ app/models.py
@@ -30,3 +30,8 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True, unique=True)
