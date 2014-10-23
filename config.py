c6aa4e3e9e15bdef8f6951f4557af1783452719a
speedcom <mateusz.maciaszekhpc@gmail.com>
2014-10-23 22:40:13 +0200

    removed pagination props from config file

--- config.py
+++ config.py
@@ -7,5 +7,3 @@ SECRET_KEY = 'you-will-never-guess'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'zpi.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
-# pagination
-OFFERS_PER_PAGE = 3
