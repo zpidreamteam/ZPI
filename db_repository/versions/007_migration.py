80ce40a6367f3197f54dfc364a24f9c78f6f4865
NicolasDabsky <nako92@gmail.com>
2014-11-10 22:56:19 +0100

    db_migration bug fix

--- db_repository/versions/007_migration.py
+++ db_repository/versions/007_migration.py
@@ -5,15 +5,43 @@ from migrate import *
 from migrate.changeset import schema
 pre_meta = MetaData()
 post_meta = MetaData()
+upload = Table('upload', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('name', Unicode(length=255), nullable=False),
+    Column('url', Unicode(length=255), nullable=False),
+)
+
+offer = Table('offer', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('name', String(length=128)),
+    Column('title', String(length=128)),
+    Column('book_author', String(length=128)),
+    Column('price', Float),
+    Column('shipping', Float),
+    Column('count', Integer),
+    Column('body', String(length=140)),
+    Column('timestamp', DateTime),
+    Column('category_id', Integer),
+    Column('user_id', Integer),
+)
+
 
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
+    post_meta.tables['upload'].create()
+    post_meta.tables['offer'].columns['book_author'].create()
+    post_meta.tables['offer'].columns['shipping'].create()
+    post_meta.tables['offer'].columns['title'].create()
 
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
+    post_meta.tables['upload'].drop()
+    post_meta.tables['offer'].columns['book_author'].drop()
+    post_meta.tables['offer'].columns['shipping'].drop()
+    post_meta.tables['offer'].columns['title'].drop()
--- db_repository/versions/011_migration.py
+++ db_repository/versions/011_migration.py
@@ -5,28 +5,15 @@ from migrate import *
 from migrate.changeset import schema
 pre_meta = MetaData()
 post_meta = MetaData()
-transaction = Table('transaction', post_meta,
-    Column('id', Integer, primary_key=True, nullable=False),
-    Column('timestamp', DateTime),
-    Column('user_id', Integer),
-    Column('offer_id', Integer),
-    Column('count', Integer),
-    Column('price', Float),
-    Column('hash_link', String(length=128)),
-    Column('is_finalised', Boolean),
-)
-
 
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
-    post_meta.tables['transaction'].create()
 
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
-    post_meta.tables['transaction'].drop()
